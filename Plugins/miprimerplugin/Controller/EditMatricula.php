da17732fd018e7a76683d9b2787d407d9bcf3fb2
carlamoreno123 <carlamorenoc2005@gmail.com>
2024-10-22 20:08:01 +0200

    ahora bien

--- Plugins/miprimerplugin/Controller/EditMatricula.php
+++ Plugins/miprimerplugin/Controller/EditMatricula.php
@@ -1,12 +1,29 @@
 <?php
+
 namespace FacturaScripts\Plugins\miprimerplugin\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
+
+use FacturaScripts\Dinamic\Model\Asignatura;
+
 
 class EditMatricula extends EditController
 {
     public function getModelClassName(): string
     {
         return 'Matricula';
+    }
+    protected function insertAction()
+    {
+        $asignatura = new Asignatura();
+        $idasignatura = $this->request->request->get('idasignatura');
+        $asignatura = $asignatura->get($idasignatura);
+        if ($asignatura->plazas == 0) {
+            return false;
+        } else if (parent::insertAction()) {
+
+            $asignatura->plazas--;
+            $asignatura->save();
+        }
     }
 }
--- Plugins/miprimerplugin/Controller/ListAlumno.php
+++ Plugins/miprimerplugin/Controller/ListAlumno.php
@@ -1,68 +1,77 @@
-<?php 
-    namespace FacturaScripts\Plugins\miprimerplugin\Controller;
+<?php
 
-    use FacturaScripts\Core\Lib\ExtendedController\ListController;
-    
-    class ListAlumno extends ListController {
-        public function getPageData(): array
-        {
-            $page = parent :: getPageData();
-            $page ['title'] ='Alumnos';
-            $page ['menu'] ='Academia';
-            $page ['icon'] ='fas fa-user-friends';
-            return $page;
-        }
+namespace FacturaScripts\Plugins\miPrimerPlugin\Controller;
 
-        
-        protected function  createViews() 
-        
-        {
-            $this->createAlumno();
-            $this->createRecibo();
-            $this->createMatricula();
-            $this->createEvaluacion();
-            
-        }
-        protected function createAlumno($viewName='ListAlumno'){
-        
-                $this->addView('ListAlumno','Alumno','Alumnos','fas fa-user-friends');
-                $this->addSearchFields('ListAlumno', ['nombre', 'dni' , 'telefono' ,'email']);
-                $this->addOrderBy('ListAlumno', ['nombre'], 'Nombre');
-                $this->addOrderBy('ListAlumno', ['fechanacimiento'], 'fechanacimiento',2);
-                $this->addFilterPeriod('ListAlumno', 'date', 'period', 'fechanacimiento');
-            }
-        protected function createRecibo($viewName='ListRecibo'){
-                $this->addView('ListRecibo', 'Recibo' , 'Recibos','fa-solid fa-file-invoice');
-                $this->addOrderBy('ListRecibo', ['fecha'], 'Fecha',2);
-                $this->addFilterPeriod('ListRecibo', 'date', 'period', 'fecha');
+use FacturaScripts\Core\Lib\ExtendedController\ListController;
 
-            }
-            protected function createMatricula($viewName='ListRecibo'){
-                $this->addView('ListMatricula', 'Matricula' , 'Matriculas','fa-solid fa-file-invoice');
-                $this->addOrderBy('ListMatricula', ['fechaalta'], 'Fechaalta',2);
-                $this->addFilterPeriod('ListMatricula', 'date', 'period', 'fechaalta');
-               
-            }
-            protected function createEvaluacion($viewName='ListEvaluacion'){
-                $this->addView('ListEvaluacion', 'Evaluacion' , 'Evaluaciones','fa-solid fa-file-invoice');
-                $this->addOrderBy('ListEvaluacion', ['fecha'], 'fecha');
-                $this->addFilterPeriod('ListEvaluacion', 'date', 'period', 'fecha');
-               
-            }
+class ListAlumno extends ListController
+{
+    public function getPageData(): array
+    {
+        $page = parent::getPageData();
+        $page['title'] = 'Alumnos';
+        $page['menu'] = 'Academia';
+        $page['icon'] = 'fas fa-user-friends';
+        //$page['showonmenu'] = false;
+        return $page;
+    }
 
-            
-        
-        
-        protected function loadData($viewName, $view)
-        {
-            switch($viewName){
-                case 'ListEvaluacion':
-                case 'ListMatricula':
-                case 'ListRecibo':
-                case 'ListAlumno':
-                    $view ->loadData();
-                    break;
-            }
-            
+    protected function createViews()
+    {
+        $this->createAlumno();
+        $this->createRecibo();
+        $this->createMatricula();
+        $this->createEvaluacion();
+    }
+
+
+    protected function createAlumno($viewName = 'ListAlumno')
+    {
+        $this->addView($viewName, 'Alumno', 'Alumnos', 'fas fa-user-friends');
+        $this->addSearchFields($viewName, ['nombre', 'dni', 'telefono', 'email']);
+        $this->addOrderBy($viewName, ['fechanacimiento'], 'Fecha de nacimiento', 2);
+        $this->addOrderBy($viewName, ['nombre'], 'Nombre');
+        $this->addFilterPeriod($viewName, 'date', 'period', 'fechanacimiento');
+    }
+
+    protected function createRecibo($viewName = 'ListRecibo')
+    {
+        $this->addView($viewName, 'Recibo', 'Recibos', 'fas fa-receipt');
+        $this->addOrderBy($viewName, ['fecha'], 'Fecha', 2);
+        $this->addFilterPeriod($viewName, 'date', 'period', 'fecha');
+    }
+
+    protected function createMatricula($viewName = 'ListMatricula')
+    {
+        $this->addView($viewName, 'Matricula', 'Matriculas', 'fas fa-id-card');
+        $this->addSearchFields($viewName, ['idalumno', 'idasignatura']);
+        $this->addOrderBy($viewName, ['fechaalta'], 'Fecha de alta', 2);
+        $this->addFilterAutocomplete($viewName, 'idalumno', 'Alumno', 'idalumno', 'alumnos', 'idalumno', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'idasignatura', 'Asignatura', 'idasignatura', 'asignaturas', 'idasignatura', 'nombre');
+        $this->addFilterPeriod($viewName, 'date', 'period', 'fechaalta');
+    }
+
+    protected function createEvaluacion($viewName = 'ListEvaluacion')
+    {
+        $this->addView($viewName, 'Evaluacion', 'Evaluaciones', 'fas fa-award');
+        $this->addOrderBy($viewName, ['fecha'], 'Fecha', 2);
+        $this->addOrderBy($viewName, ['nota'], 'Nota');
+        $this->addOrderBy($viewName, ['comportamiento'], 'Comportamiento');
+        $this->addFilterPeriod($viewName, 'date', 'period', 'fecha');
+        $this->addFilterAutocomplete($viewName, 'idalumno', 'Alumno', 'idalumno', 'alumnos', 'idalumno', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'idasignatura', 'Asignatura', 'idasignatura', 'asignaturas', 'idasignatura', 'nombre');
+        $this->addFilterAutocomplete($viewName, 'idprofesor', 'Profesor', 'idprofesor', 'profesores', 'idprofesor', 'nombre');
+    }
+
+    protected function loadData($viewName, $view)
+    {
+        switch ($viewName) {
+            case 'ListRecibo':
+            case 'ListMatricula':
+            case 'ListAlumno':
+            case 'ListEvaluacion':
+                $view->loadData();
+                break;
         }
     }
+}
